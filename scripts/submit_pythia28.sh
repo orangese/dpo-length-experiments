a5328acb2a5c1c10a9cd415fb920504841411e12
Ryan Park <rypark@iris2.stanford.edu>
2023-10-27 09:43:17 -0700

    Add slurm scripts

---
+++ scripts/submit_pythia28.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=iris
+#SBATCH --partition=iris-hi
+#SBATCH --nodes=1
+#SBATCH --mem=200G
+#SBATCH --gres=gpu:titanrtx:6
+#SBATCH --job-name=pythia28-rlcd-base
+#SBATCH --output slurm/%j.out
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=rypark@stanford.edu
+
+source env/bin/activate
+ulimit -n 64000
+python -u train.py model=pythia28 batch_size=6 eval_batch_size=6 trainer=FSDPTrainer
