4dd2c4fe606c3d2c1fcf95db501af8866b9bc300
Ryan Park <rypark@stanford.edu>
2023-11-07 23:27:28 -0800

    Update scripts and sampled

--- scripts/submit_sample.sh
+++ scripts/submit_sample.sh
@@ -14,5 +14,5 @@
 
 source env/bin/activate
 ulimit -n 64000
-#python -u train.py model.archive=/iris/u/rafailov/DPOExperiments/models/rafailov/pythia2.8b_sft_hh/LATEST/policy.pt eval_batch_size=4
-python -u train.py model.archive=/iris/u/rypark/cache/rypark/pythia28-hh-base_2023-11-03_15-41-09_113519/step-180000/policy.pt eval_batch_size=4
+python -u train.py model.archive=/iris/u/rafailov/DPOExperiments/models/rafailov/pythia2.8b_sft_hh/LATEST/policy.pt eval_batch_size=12 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_hh_sft.json exp_name=pythia28-hh-sft-sample n_eval_examples=1000 n_eval_model_samples=100 debug=true trainer=BasicTrainer datasets=[hh]
+python -u train.py model.archive=/iris/u/rypark/cache/rypark/pythia28-hh-base_2023-11-03_15-41-09_113519/step-180000/policy.pt eval_batch_size=12 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_hh_base_dpo.json exp_name=pythia28-hh-base-dpo-sample n_eval_examples=1000 n_eval_model_samples=100 debug=true trainer=BasicTrainer datasets=[hh]
