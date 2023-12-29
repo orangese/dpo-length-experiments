b404f19d1f31269201a535d207d04f945838780d
Ryan Park <rypark@stanford.edu>
2023-12-28 21:17:42 -0800

    Update scripts and submitter

---
+++ scripts/shp/mod_005_pythia28.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=iris
+#SBATCH --partition=iris-hi
+#SBATCH --nodes=1
+#SBATCH --mem=200G
+#SBATCH --gres=gpu:a6000:4
+#SBATCH --time=240:00:00
+#SBATCH --job-name=shp-pythia28-mod-005
+#SBATCH --output slurm/%j.out
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=rypark@stanford.edu
+
+source env/bin/activate
+ulimit -n 64000
+python -u train.py model=pythia28 batch_size=128 eval_batch_size=4 trainer=FSDPTrainer model.archive=/iris/u/rafailov/DPOExperiments/models/rafailov/pythia2.8b_sft_shp/LATEST/policy.pt model.fsdp_policy_mp=bfloat16 gradient_accumulation_steps=16 loss=dpo loss.beta=0.1 loss.alpha=0.005 datasets=[shp] exp_name=shp-pythia28-mod-005
--- submit.py
+++ submit.py
@@ -6,39 +6,46 @@ if __name__ == "__main__":
     print(msg)
 
     sample = input("are you sampling (y/N)? ")
+    dirs_ = [d for d in os.listdir('scripts') if os.path.isdir(os.path.join("scripts", d))]
+    ds = input(f"choose dataset {dirs_}: ")
+    if ds not in dirs_:
+        print("ERROR: selected not in directories")
+        sys.exit(1)
+    
     if sample.lower() == "y":
-        f = "scripts/sample.sh"
+        f = f"scripts/{ds}/sample.sh"
 
-    else:
-        dirs_ = [d for d in os.listdir('scripts') if os.path.isdir(os.path.join("scripts", d))]
-        ds = input(f"choose dataset {dirs_}: ")
-        if ds not in dirs_:
-            print("ERROR: selected not in directories")
-            sys.exit(1)
-       
-        model = input(f"choose model (pythia28): ")
-        if not model:
-            model = "pythia28"
-
-        mod = input("add length mod (y/N)? ")
-        f = f"scripts/{ds}/{'base' if not mod else 'mod'}_{model}.sh"
+    else:   
+        model = input(f"choose model (pythia28): ") or "pythia28"
+        alpha = input("length penalty (0)? ") or "0.0"
+        
+        f = f"scripts/{ds}/mod_{alpha[2:]}_{model}.sh"
         if not os.path.exists(f):
             print(f"ERROR: {f} does not exist")
             sys.exit(1)
 
     print("\nprinting requested file below...")
     print("=" * 80)
+    ds_found = set()
     with open(f, "r") as fb:
         for line in fb:
             start, end = "\033[1m", "\033[0;0m"
             if line.startswith("#"):
                 start, end = "\x1B[3m", "\x1B[0m"
             print(start + line.strip() + end)
+            for dir_ in dirs_:
+                if dir_ in line:
+                    ds_found.add(dir_)
             
     print("=" * 80)
+    print(f"datasets found: {ds_found}")
+    
+    if len(ds_found) != 1 or list(ds_found)[0] != ds:
+        print("ERROR: datasets found in job file != requested ds")
+        sys.exit(1)
 
     cmd = f"sbatch {f}" 
     msg = f"Execute '{cmd}' (Y/n)? "
     if input(msg).lower() != "n":
         os.system(cmd)
-        os.system("watch squeue -u $USER")
+        #os.system("watch squeue -u $USER")
