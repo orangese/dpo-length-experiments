50ef57a1a841e249d361d2754ffcbc7a4849fc84
Ryan Park <rypark@stanford.edu>
2024-01-03 06:51:11 -0800

    Add shp alpha005 sample

--- scripts/shp/sample.sh
+++ scripts/shp/sample.sh
@@ -20,13 +20,13 @@ ulimit -n 64000
 
 #### LENGTH DPO SAMPLING
 # alpha = 0.005
-#python -u train.py model.archive= eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_alpha005.json exp_name=pythia28-shp-alpha005-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
+python -u train.py model.archive=/iris/u/rypark/cache/rypark/shp-pythia28-mod-005_2023-12-28_19-22-43_109845/step-199680/policy.pt eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_alpha005.json exp_name=pythia28-shp-alpha005-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
 
 # alpha = 0.01
-python -u train.py model.archive=/iris/u/rypark/cache/rypark/shp-pythia28-mod-01_2023-12-27_22-12-47_942163/step-199680/policy.pt eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_alpha01.json exp_name=pythia28-shp-alpha01-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
+#python -u train.py model.archive=/iris/u/rypark/cache/rypark/shp-pythia28-mod-01_2023-12-27_22-12-47_942163/step-199680/policy.pt eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_alpha01.json exp_name=pythia28-shp-alpha01-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
 
 # alpha = 0.02
 #python -u train.py model.archive= eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_alpha02.json exp_name=pythia28-shp-alpha02-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
 
 #### BASE DPO SAMPLING
-python -u train.py model.archive=/iris/u/rypark/cache/rypark/shp-pythia28-mod-0_2023-12-27_12-09-55_579462/step-179712/policy.pt eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_base_dpo_full.json exp_name=pythia28-shp-base-dpo-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
+#python -u train.py model.archive=/iris/u/rypark/cache/rypark/shp-pythia28-mod-0_2023-12-27_12-09-55_579462/step-179712/policy.pt eval_batch_size=16 sample_only=true samples_per_prompt=1 sample_path=/sailhome/rypark/dpo-length-experiments/sampled/pythia28_shp_base_dpo_full.json exp_name=pythia28-shp-base-dpo-sample n_eval_examples=10000 n_eval_model_samples=10000 debug=true trainer=BasicTrainer datasets=[shp]
